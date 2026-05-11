68a2b479f82042d2605ddcfeabfc03ca2ba8c2a0
VCZAPATA <VCZAPATA@HOTMAIL.COM>
2026-05-11 23:56:24 +0200

    Primer commit

---
+++ src/enjambre_drones/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'enjambre_drones'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')
+        ),
+        (
+            os.path.join('share', package_name, 'urdf'),
+            glob('urdf/*')
+        ),
+        (
+        os.path.join('share', package_name, 'meshes'),
+        glob('meshes/*')
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Tu Nombre',
+    maintainer_email='tu_correo@ejemplo.com',
+    description='Paquete de descripción y visualización del dron base del TFG',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
